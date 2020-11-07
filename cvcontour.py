20013737610f83fdb9bcc1ada9b19e2820eb7d2d
checko <checko@gmail.com>
2020-11-08 00:34:52 +0800

    thresh OK

--- cvcontour.py
+++ cvcontour.py
@@ -11,4 +11,12 @@ image = cv2.imread(args["image"])
 gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 cv2.imshow('gray',gray)
 cv2.waitKey(0)
+
+blurred = cv2.GaussianBlur(gray,(5,5),0)
+cv2.imshow('blurred',blurred)
+cv2.waitKey(0)
+
+thresh = cv2.threshold(blurred,60,255,cv2.THRESH_BINARY)[1]
+cv2.imshow('thresh',thresh)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
