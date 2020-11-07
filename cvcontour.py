e7ec0e17b5bcfa54930b5a87b0099453fa65d84f
checko <checko@gmail.com>
2020-11-08 01:15:45 +0800

    OK version

--- cvcontour.py
+++ cvcontour.py
@@ -19,4 +19,21 @@ cv2.waitKey(0)
 thresh = cv2.threshold(blurred,60,255,cv2.THRESH_BINARY)[1]
 cv2.imshow('thresh',thresh)
 cv2.waitKey(0)
+
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+
+for c in cnts:
+    M = cv2.moments(c)
+    if M["m00"] > 100:
+        cX = int(M["m10"]/M["m00"])
+        cY = int(M["m01"]/M["m00"])
+
+        cv2.drawContours(image,[c],-1,(0,255,0),2)
+        cv2.circle(image,(cX,cY),7,(255,255,255),-1)
+        cv2.putText(image,"center",(cX-20,cY-20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),2)
+
+        cv2.imshow("Image",image)
+        cv2.waitKey(0)
+
 cv2.destroyAllWindows()
