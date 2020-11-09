05f0d2e035918e9a88cab23152c89fd48cbec2f6
charles.chang <charles.chang@royaltek.com>
2020-11-09 15:27:53 +0800

    cpp implement OK

--- cvcontour.cpp
+++ cvcontour.cpp
@@ -7,9 +7,9 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	Mat image;
-	image	= imread(argv[1],CV_LOAD_IMAGE_UNCHANGED);
-	cvtColor(image,image, COLOR_BGR2GRAY);
+	Mat image,srcimage;
+	srcimage	= imread(argv[1],CV_LOAD_IMAGE_UNCHANGED);
+	cvtColor(srcimage,image, COLOR_BGR2GRAY);
 	namedWindow("gray", CV_WINDOW_AUTOSIZE);
 	imshow("gray",image);
 
@@ -20,6 +20,32 @@ int main(int argc, char** argv)
 	threshold(image,image,60,255,CV_THRESH_BINARY);
 	namedWindow("thresh",CV_WINDOW_AUTOSIZE);
 	imshow("thresh",image);
+
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+	findContours(image, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
+
+	vector<Moments> mu(contours.size());
+	for(int i=0;i<contours.size();i++)
+	{
+		mu[i] = moments( contours[i], false);
+	}
+
+	vector<Point2f> mc( contours.size());
+	for(int i=0;i<contours.size();i++)
+	{
+		if(mu[i].m00 > 100) {
+			mc[i] = Point2f(mu[i].m10/mu[i].m00, mu[i].m01/mu[i].m00);
+	
+			drawContours(srcimage, contours, i,(0,255,255,0),3);
+			circle(srcimage, mc[i], 4, (255,255,255),-1,7,0);
+		}
+	}
+	namedWindow("cont",CV_WINDOW_AUTOSIZE);
+	imshow("cont",srcimage);
+
+
+
 	
 	
 	waitKey(0);
